41e31b38d6e2e3d6320209055b4fc5d6364efb14
Yusuf Ulaş Yıldız <y.ulas.yildiz@gmail.com>
2025-05-24 11:20:49 +0300

    init hash update

--- script/deployParameters/DeployMonad.s.sol
+++ script/deployParameters/DeployMonad.s.sol
@@ -11,7 +11,7 @@ contract DeployMonad is DeployUniversalRouter {
             weth9: 0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701,
             v2Factory: 0xe26dd94f67Ca3615fcaF6062750147F37Df84F7a,
             v3Factory: 0xD404cbEE8213aD72f545c07fA47c320CAB86677b,
-            pairInitCodeHash: hex"1fa70eb1d54eb2007acbf24d7a22d171e4d7d83f736b11d5628c4c35a82a2f00",
+            pairInitCodeHash: 0x01fa70eb1d54eb2007acbf24d7a22d171e4d7d83f736b11d5628c4c35a82a2f0,
             poolInitCodeHash: 0xc0ccad81c22e0bf0c1c263ac97e317437010ea69d9ee4ceb18ceda22c27098ee,
             v4PoolManager: address(0),
             v3NFTPositionManager: 0x792116870fB00965ebC81F05F4B9a364aF9d2EFe,
